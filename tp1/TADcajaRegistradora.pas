2d35085c7c0ad09e128e244ef6493402c16d39dc
nahueqs <qsnahuel@gmail.com>
2022-04-01 17:49:13 -0300

    ej 9 listo

--- tp1/TADcajaRegistradora.pas
+++ tp1/TADcajaRegistradora.pas
@@ -4,7 +4,7 @@ interface
 
 Uses
 
-  SysUtils, Math , System.Variants, System.Classes;
+  SysUtils, Math , System.Variants, System.Classes, Vcl.StdCtrls;
 
 type
   TError = (exitoso, erroneo);
@@ -17,7 +17,7 @@ type
       procedure Cargar(billACargar:arraybilletes);
       function Saldo(billSaldo:arraybilletes):real;
       function DarVuelto( var billetesVuelto , billetesPago:arraybilletes;pago:real):TError;
-//      CerrarCaja
+      function CerrarCaja():string;
   end;
 
 const
@@ -32,7 +32,7 @@ var
   I: EnumBilletes;
 begin
   for I := b1 to b1000 do
-    billetesCaja[I] := billACargar[i];
+    billetesCaja[I] := billetesCaja[I] + billACargar[i];
 end;
 
 function caja.Saldo(billSaldo: arrayBilletes):real;
@@ -49,9 +49,57 @@ end;
 function caja.DarVuelto(VAR billetesVuelto, billetesPago :arrayBilletes; pago: Real):TError;
 var
   i:enumBilletes;
+  cantidadAPAgar:real;
+  error:TError;
+  vAux:arrayBilletes;
 begin
+  vAux := billetesCaja;
+  for I := B1 to B1000 do
+    billetesVuelto[I] := 0;
+
+  Cargar(billetesPago);
+
+  cantidadAPAgar := saldo(billetesPago) - pago ;
+
+  error:=exitoso;
+
+  while (cantidadAPagar > 0) and (error = exitoso) do begin
+    for I := B1000 downto B1 do
+      while (cantidadAPagar >= denominacion[I]) and (BilletesCaja[i] > 0) do begin
+        billetesVuelto[I] := billetesVuelto[I] + 1;
+        cantidadAPAgar := cantidadAPAgar - denominacion[i];
+        billetesCaja[i] := billetesCaja[i] - 1;
+      end;
+      if cantidadAPAgar <> 0 then
+          error := erroneo
+      else
+        billetesCaja := vAux;
+  end;
+
+  result := error;
 
 end;
+
+function caja.CerrarCaja():string;
+var
+  strAux:string;
+  I:enumbilletes;
+begin
+  strAux:='La composicion de la caja final es: ' + sLineBreak;
+  strAux:= strAux + 'Monedas: ' + sLineBreak;
+  for I := B1 to B5 do
+    strAux := strAux + denominacion[i].ToString + ' : ' + Billetescaja[i].ToString  + sLineBreak;
+  strAux:= strAux + 'Billetes: ' + sLineBreak;
+  for I := B10 to B1000 do
+    strAux := strAux + denominacion[i].ToString + ' : ' + Billetescaja[i].ToString  + sLineBreak;
+  strAux:= strAux + 'Con un saldo total de: ' + floattoStr(Saldo(Billetescaja));
+
+  result := strAux;
+end;
+
+
+
+
 
 
 
--- tp1/ej9_form.dfm
+++ tp1/ej9_form.dfm
@@ -146,6 +146,7 @@ object Formej9: TFormej9
     Height = 58
     Caption = 'Ingresar'
     TabOrder = 12
+    OnClick = btnIngresarClick
   end
   object btnCobrar: TButton
     Left = 110
@@ -154,6 +155,7 @@ object Formej9: TFormej9
     Height = 56
     Caption = 'Cobrar'
     TabOrder = 13
+    OnClick = btnCobrarClick
   end
   object inputMontoACobrar: TLabeledEdit
     Left = 110
@@ -307,6 +309,7 @@ object Formej9: TFormej9
     Height = 57
     Caption = 'Cerrar'
     TabOrder = 26
+    OnClick = btnCerrarClick
   end
   object btnVerSaldo: TButton
     Left = 336
