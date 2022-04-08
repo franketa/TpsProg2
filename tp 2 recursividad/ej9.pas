4ea520ccc8f068b60aab7b2cfe5a3ca57ce70d38
nahueqs <qsnahuel@gmail.com>
2022-04-08 01:56:54 -0300

    ej 9 inprogress 3

--- tp 2 recursividad/ej9.pas
+++ tp 2 recursividad/ej9.pas
@@ -37,10 +37,10 @@ const
    grilla : array[min..max, min..max] of String =
    (
     ('C','C','C','C',' ',' ',' ',' ',' ',' '),
-    ('',' ',' ','C',' ','C',' ',' ',' ',' '),
-    ('',' ',' ','C','C','C',' ',' ',' ',' '),
-    ('',' ',' ','C',' ','C',' ',' ',' ',' '),
-    ('',' ',' ',' ',' ','C',' ',' ',' ',' '),
+    (' ',' ','C','',' ','',' ',' ',' ',' '),
+    (' ',' ','C','','','',' ',' ',' ',' '),
+    ('',' ',' ','',' ','',' ',' ',' ',' '),
+    ('',' ',' ',' ',' ','',' ',' ',' ',' '),
     ('',' ',' ',' ',' ',' ',' ',' ',' ',' '),
     ('',' ',' ',' ',' ',' ',' ',' ',' ',' '),
     ('',' ',' ',' ',' ',' ',' ',' ',' ',' '),
@@ -62,117 +62,125 @@ begin
   if (fila = pos2.fila) and (col = pos2.col) then result := true;
 end;
 
-function setVectorVectores():arrayarrayPosiciones;
+function setVectorVectores(var memo:Tmemo):arrayarrayPosiciones;
 var
-  indiceArrayArray:integer;
+  indiceArrayArray,i,j:integer;
   arrayDeArraysResultado:arrayarrayPosiciones;
   posInicial,posAnteriorInicial:posicion;
-
+  strAux:string;
   function getVectorPosicionesCarbonos(posActual, posAnterior:posicion; auxVectorResultado:arrayPosiciones; indiceAuxVectorResultado:integer):arrayPosiciones;
     var
       contador:integer;
     procedure llenarArrayPosicionesCarbono(posActual,posAnterior:posicion);
-  var
-    pos1, pos2, posSiguiente:posicion;
-  begin
-    // inicializo
-    pos1.fila := -1;
-    pos2.fila := -1;
-    contador := 0;
-    // asigno el actual al vector
-    if posActual.fila <> -1 then begin
-      setLength(auxVectorResultado,indiceAuxVectorResultado + 1); // aumento el largo del vResultado en 1
-      auxVectorResultado[indiceAuxVectorResultado] := posActual; // asigno la posicion en el ultimo del vResultado
-      inc(indiceAuxVectorResultado); // aumento el indice del vResultado
+    var
+      pos1, pos2, posSiguiente:posicion;
+    begin
+      // inicializo
+      pos1.fila := -1;
+      pos2.fila := -1;
+      contador := 0;
+      // asigno el actual al vector
+      if posActual.fila <> -1 then begin
+        setLength(auxVectorResultado,indiceAuxVectorResultado + 1); // aumento el largo del vResultado en 1
+        auxVectorResultado[indiceAuxVectorResultado] := posActual; // asigno la posicion en el ultimo del vResultado
+        inc(indiceAuxVectorResultado); // aumento el indice del vResultado
 
-      posSiguiente := posActual;
-      inc(posSiguiente.col);
-      // caso misma fila columna siguiente
-      if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior)) and (posSiguiente.col < max) then begin
-        if contador = 1 then begin
-          pos2 := posSiguiente;
-          setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
-          arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado);; // asigno la posicion en el ultimo del vResultado
-          inc(indiceArrayArray);
+        posSiguiente := posActual;
+        inc(posSiguiente.col);
+        // caso misma fila columna siguiente
+        if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior)) and (posSiguiente.col < max) then begin
+          if contador = 1 then begin
+            pos2 := posSiguiente;
+            setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
+            arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado); // asigno la posicion en el ultimo del vResultado
+            inc(indiceArrayArray);
+          end;
+          if contador = 0 then begin
+            pos1 := posSiguiente;
+            inc(contador);
+          end;
         end;
-        if contador = 0 then begin
-          pos1 := posSiguiente;
-          inc(contador);
+        posSiguiente := posActual;
+        dec(posSiguiente.col);
+        // caso misma fila columna anterior
+        if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior))and (posSiguiente.col > min) then begin
+          if contador = 1 then begin
+            pos2 := posSiguiente;
+            setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
+            arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado); // asigno la posicion en el ultimo del vResultado
+            inc(indiceArrayArray);
+          end;
+          if contador = 0 then begin
+            pos1 := posSiguiente;
+            inc(contador);
+          end;
         end;
+        // caso misma columna fila arriba
+        posSiguiente := posActual;
+        dec(posSiguiente.fila);
+        if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior))and (posSiguiente.fila > min) then begin
+          if contador = 1 then begin
+            pos2 := posSiguiente;
+            setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
+            arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado); // asigno la posicion en el ultimo del vResultado
+            inc(indiceArrayArray);
+          end;
+          if contador = 0 then begin
+            pos1 := posSiguiente;
+            inc(contador);
+          end;
+        end;
+        // caso misma columna fila abajo
+        posSiguiente := posActual;
+        inc(posSiguiente.fila);
+        if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior)) and (posSiguiente.fila < max) then begin
+          // MODIFICAR, CUANDO getVectorPosicionesCarbonos no sigue llenando la cadena al vectordevectores
+          if contador = 1 then begin
+            pos2 := posSiguiente;
+            setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
+            arrayDeArraysResultado[indiceArrayArray] := auxVectorResultado;
+            arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,arrayDeArraysResultado[indiceArrayArray], indiceArrayArray); // asigno la posicion en el ultimo del vResultado
+
+          end;
+          if contador = 0 then begin
+            pos1 := posSiguiente;
+            inc(contador);
+          end;
+        end;
+        // ya tengo la o las posiciones de las c siguientes
+        llenarArrayPosicionesCarbono(pos1,posActual);
       end;
-      posSiguiente := posActual;
-      dec(posSiguiente.col);
-      // caso misma fila columna anterior
-      if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior))and (posSiguiente.col > min) then begin
-        if contador = 1 then begin
-          pos2 := posSiguiente;
-          setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
-          arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado);; // asigno la posicion en el ultimo del vResultado
-          inc(indiceArrayArray);
-        end;
-        if contador = 0 then begin
-          pos1 := posSiguiente;
-          inc(contador);
-        end;
-      end;
-      // caso misma columna fila arriba
-      posSiguiente := posActual;
-      dec(posSiguiente.fila);
-      if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior))and (posSiguiente.fila > min) then begin
-        if contador = 1 then begin
-          pos2 := posSiguiente;
-          setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
-          arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado);; // asigno la posicion en el ultimo del vResultado
-          inc(indiceArrayArray);
-        end;
-        if contador = 0 then begin
-          pos1 := posSiguiente;
-          inc(contador);
-        end;
-      end;
-      // caso misma columna fila abajo
-      posSiguiente := posActual;
-      inc(posSiguiente.fila);
-      if (grilla[posSiguiente.fila, posSiguiente.col] = 'C') and not(posSiguiente.esIgualA(posAnterior)) and (posSiguiente.fila < max) then begin
-        if contador = 1 then begin
-          pos2 := posSiguiente;
-          setLength(arrayDeArraysResultado,indiceArrayArray + 1); // aumento el largo del vResultado en 1
-          arrayDeArraysResultado[indiceArrayArray] := getVectorPosicionesCarbonos(pos2,posActual,auxVectorResultado, indiceAuxVectorResultado);; // asigno la posicion en el ultimo del vResultado
-          inc(indiceArrayArray);
-        end;
-        if contador = 0 then begin
-          pos1 := posSiguiente;
-          inc(contador);
-        end;
-      end;
-      // ya tengo la o las posiciones de las c siguientes
-      llenarArrayPosicionesCarbono(pos1,posActual);
     end;
-    // asigno al vector vectores posiciones
-    setLength(auxVectorResultado,indiceAuxVectorResultado + 1); // aumento el largo del vResultado en 1
-    auxVectorResultado[indiceAuxVectorResultado] := posActual; // asigno la posicion en el ultimo del vResultado
-    inc(indiceAuxVectorResultado);
-  end;
-  begin
-    llenarArrayPosicionesCarbono(posActual, posAnterior);
-    result := auxVectorResultado;
-  end;
+    begin
+      llenarArrayPosicionesCarbono(posActual, posAnterior);
+      result := auxVectorResultado;
+    end;
   // funcion auxiliar fin
 // funcion principal
 begin
-  indiceArrayArray := 1;
+  indiceArrayArray := 0;
   posInicial.fila := 1;
   posInicial.col := 1;
   posAnteriorInicial.fila := -1;
   posAnteriorInicial.col := -1;
   setLength(arrayDeArraysResultado, 1);
+  strAux := '';
   arrayDeArraysResultado[0] := getVectorPosicionesCarbonos(posInicial, posAnteriorInicial, arrayDeArraysResultado[0], indiceArrayArray);
+  for i := 0 to length(arrayDeArraysResultado)-1 do begin
+    for j := 0 to length(arrayDeArraysResultado[i])-1 do begin
+      strAux:= strAux + ' (' + arrayDeArraysResultado[i][j].fila.ToString + ':' + arrayDeArraysResultado[i][j].col.ToString + ') ';
+    end;
+    memo.Lines.Add(strAux);
+  end;
+
+
+
 end;
 
 
 procedure Tformej9.Button1Click(Sender: TObject);
 begin
-   setVectorVectores();
+   setVectorVectores(memo1);
 end;
 
 end.
