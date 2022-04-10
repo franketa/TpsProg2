b5505693821888388232da25eca3c629f38959c4
franketa <francohbalik@gmail.com>
2022-04-09 23:45:12 -0300

    ej10 ongoing

--- tp 2 recursividad/ej10.pas
+++ tp 2 recursividad/ej10.pas
@@ -28,29 +28,39 @@ implementation
 
 {$R *.dfm}
 
-function hallarConjuntos(conjuntoDefault:conjunto;num,i:integer):vectorConjuntos;
+function hallarConjuntos(conjuntoDefault:conjunto;numObjetivo,i:integer):vectorConjuntos;
 var
   j:integer;
+  z:integer;
   vAux:conjunto;
+  vAux2:conjunto;
 begin
 
-  if conjuntoDefault[i] = num then begin
+  if (conjuntoDefault[i] = numObjetivo) then begin
+
     setLength(vAux, length(conjuntoDefault) - 1);
+    setLength(vAux2, 1);
     for j := i + 1 to length(conjuntoDefault) do begin
       vAux[j] := conjuntoDefault[j];
     end;
-    result[i] := conjuntoDefault[i];
+    vAux2[1]:=conjuntoDefault[i];
+    result[i] := vAux2;
     inc(i);
-    hallarConjuntos(vAux, num, i);
+    for z := 0 to length(vAux2)-1 do begin
+      if vAux2[z]=numObjetivo then begin
+          vAux2[z]:=0
+      end;
+    end;
+    hallarConjuntos(vAux, numObjetivo, i);
   end;
-  if conjuntoDefault[i] < num then begin
+  if conjuntoDefault[i] < numObjetivo then begin
     setLength(vAux, length(conjuntoDefault) - 1);
     for j := i + 1 to length(conjuntoDefault) do begin
       vAux[j] := conjuntoDefault[j];
     end;
 
     for j := 0 to length(vAux) do begin
-      if ((conjuntoDefault[i] + vAux[j]) = num) then
+      if ((conjuntoDefault[i] + vAux[j]) = numObjetivo) then
     end;
 
   end;
