90de96525ad7cca2844c27e61398b5bd7bfc3a20
franketa <francohbalik@gmail.com>
2022-04-23 18:29:46 -0300

    ej 7

--- tp3/tp3_proyect.dpr
+++ tp3/tp3_proyect.dpr
@@ -5,13 +5,15 @@ uses
   Unit1 in 'Unit1.pas' {Form1},
   ListArray in 'ListArray.pas',
   ListPointer in 'ListPointer.pas',
-  Tipos in 'Tipos.pas';
+  Tipos in 'Tipos.pas',
+  ej7 in 'ej7.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
